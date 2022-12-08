95bd6de92362e1b94b4207604563805e6ad4ab40
Moritz Gobbert <gobbert@uni-trier.de>
2022-12-08 09:53:51 +0100

    Initial commit

---
+++ ClearChatOnAltP.lua
@@ -0,0 +1,12 @@
+﻿for i = 1, NUM_CHAT_WINDOWS do
+	_G["ChatFrame"..i.."EditBox"]:HookScript("OnKeyDown", function(self, key)
+		if key == "P" and IsControlKeyDown() and IsAltKeyDown() then
+			local text = self:GetText()
+			if IsShiftKeyDown()
+				C_Timer.After(0, function() self:SetText(text.."Ö") end)
+			elseif
+				C_Timer.After(0, function() self:SetText(text.."ö") end)
+			end
+		end
+	end)
+end
